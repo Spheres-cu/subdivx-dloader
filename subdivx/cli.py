278cbd931998750237a8baa8d401b6d349851e3b
Spheres_cu <72468068+Spheres-cu@users.noreply.github.com>
2021-11-15 12:54:17 -0500

    Update cli.py
    - Redited cli.py, now you no longer need lib.py, all necessary modules and dependencies are in cli.py
    
    -Added the colorama module to enable the color output of texts in multiplatform (Win, Linux, Mac)
    
    -Now allows you to cancel the download in "choose" mode (-c), also avoid entering an invalid number in the options. Also exit with some keyboard interrupt (Ex. Control + C in Win)
    
    -An error is thrown if you do not have access to the Internet when searching for subtitles.

--- subdivx/cli.py
+++ subdivx/cli.py
@@ -1,14 +1,175 @@
 #!/bin/env python
 
 import os
+import sys
 import logging
 import argparse
-from collections import namedtuple
+import requests 
+import urllib.parse
+import textwrap as tr
 import logging.handlers
-from contextlib import contextmanager
+from colorama import init
 from guessit import guessit
+from bs4 import BeautifulSoup
+from rarfile import is_rarfile
+from collections import namedtuple
 from tvnamer.utils import FileFinder
-from . import lib
+from contextlib import contextmanager
+from tempfile import NamedTemporaryFile
+from zipfile import is_zipfile, ZipFile
+
+init()
+
+PYTHONUTF8=1
+
+SUBDIVX_SEARCH_URL = "https://www.subdivx.com/index.php"
+
+SUBDIVX_DOWNLOAD_MATCHER = {'name':'a', 'rel':"nofollow", 'target': "new"}
+
+LOGGER_LEVEL = logging.INFO
+LOGGER_FORMATTER = logging.Formatter('%(asctime)-25s %(levelname)-8s %(name)-29s %(message)s', '%Y-%m-%d %H:%M:%S')
+
+s = requests.Session()
+
+class NoResultsError(Exception):
+    pass
+
+def setup_logger(level):
+    global logger
+
+    logger = logging.getLogger()
+    """
+    logfile = logging.handlers.RotatingFileHandler(logger.name+'.log', maxBytes=1000 * 1024, backupCount=9)
+    logfile.setFormatter(LOGGER_FORMATTER)
+    logger.addHandler(logfile)
+    """
+    logger.setLevel(level)
+
+
+def get_subtitle_url(title, number, metadata, choose=False):
+    #Filter the title to avoid 's in names
+    title_f = [ x for x in title.split() if "\'s" not in x ]
+    title = ' '.join(title_f)
+    buscar = f"{title} {number}"
+    params = {"accion": 5,
+     "subtitulos": 1,
+     "realiza_b": 1,
+     "oxdown": 1,
+     "buscar": buscar ,
+    }
+    s.headers.update({"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0"})
+    try:
+        page = s.get(SUBDIVX_SEARCH_URL, params=params).text
+    except OSError:
+        print("\n \033[31m [Error,", "Connection error! \033[0m", "Unable to reach https://www.subdivx.com servers!\n\n \033[0;33m Please check:\033[0m\n" + \
+                "- Your Internet connection\n" + \
+                "- Your Firewall connections\n" + \
+                "- www.subdivx.com availability\n")
+        sys.exit(1)
+
+    soup = BeautifulSoup(page, 'html5lib')
+    titles = soup('div', id='menu_detalle_buscador')
+
+    # only include results for this specific serie / episode
+    # ie. search terms are in the title of the result item
+    descriptions = {
+        t.nextSibling(id='buscador_detalle_sub')[0].text: t.next('a')[0]['href'] for t in titles
+        if all(word.lower() in t.text.lower() for word in buscar.split())
+    }
+
+    if not descriptions:
+        raise NoResultsError(f'No suitable subtitles were found for: "{buscar}"')
+
+    # then find the best result looking for metadata keywords
+    # in the description
+    scores = []
+    for description in descriptions:
+
+        score = 0
+        for keyword in metadata.keywords:
+            if keyword in description:
+                score += 1
+        for quality in metadata.quality:
+            if quality in description:
+                score += 1.1
+        for codec in metadata.codec:
+            if codec in description:
+                score += .75
+        scores.append(score)
+
+    results = sorted(zip(descriptions.items(), scores), key=lambda item: item[1], reverse=True)
+    # Print video infos
+    print("\n\033[33m>> Subtítulo: " + str(title) + " " + str(number).upper() + "\n\033[0m")
+
+    if (choose):
+        count = 0
+        for item in (results):
+            print ("  \033[92m [%i] ===> \033[0m %s \n" % (count , tr.fill("% s" %item[0][0], width=110)))
+            count = count +1
+        print("\033[31m [" + str(count) + "] \033[0m Cancelar descarga\n")
+        res = -1
+        while (res < 0 or res > count):
+            try:
+               res = int(input (">> Elija un [#] en las opciones. Enter para la [0]: ") or "0")
+            except KeyboardInterrupt:
+                print("\n\n \033[31m Interrupto por el usuario...\033[0m")
+                sys.exit(1)
+            except:
+                res = -1
+        if (res == count):
+            print("\n \033[31m Cancelando descarga...\033[0m")
+            sys.exit(0)
+        url = (results[res][0][1]).encode("utf-8")
+    else:
+        # get subtitle page
+        url = (results[0][0][1]).encode("utf-8")
+    logger.info(f"Getting from {url}")
+    page = s.get(url).text
+    s.headers.update({"referer":url})
+    soup = BeautifulSoup(page, 'html5lib')
+    # get download link
+    return soup('a', {"class": "link1"})[0]["href"]
+
+
+def get_subtitle(url, path):
+    temp_file = NamedTemporaryFile(delete=False)
+    logger.info(f"downloading https://www.subdivx.com/{url}")
+    
+    temp_file.write(s.get('https://www.subdivx.com/' + url).content)
+    temp_file.seek(0)
+    
+    if is_zipfile(temp_file.name):
+        zip_file = ZipFile(temp_file)
+        for name in zip_file.infolist():
+            # don't unzip stub __MACOSX folders
+            if '.srt' in name.filename and '__MACOSX' not in name.filename:
+                logger.info(' '.join(['Unpacking zipped subtitle', name.filename, 'to', os.path.dirname(path)]))
+                zip_file.extract(name, os.path.dirname(path))
+
+        zip_file.close()
+
+    elif (is_rarfile(temp_file.name)):
+        rar_path = path + '.rar'
+        logger.info('Saving rared subtitle as %s' % rar_path)
+        with open(rar_path, 'wb') as out_file:
+            out_file.write(temp_file.read())
+
+        try:
+            import subprocess
+            #extract all .srt in the rared file
+            ret_code = subprocess.call(['unrar', 'e', '-inul', '-n*srt', '-n*txt', rar_path])
+            if ret_code == 0:
+                logger.info('Unpacking rared subtitle to %s' % os.path.dirname(path))
+                os.remove(rar_path)
+        except OSError:
+            logger.info('Unpacking rared subtitle failed.'
+                        'Please, install unrar to automate this step.')
+    else:
+        logger.info(f"unknown file type")
+
+
+    temp_file.close()
+    os.unlink(temp_file.name)
 
 _extensions = [
     'avi', 'mkv', 'mp4',
@@ -70,7 +231,6 @@ _codecs = ('xvid', 'x264', 'h264', 'x265')
 
 Metadata = namedtuple('Metadata', 'keywords quality codec')
 
-
 def extract_meta_data(filename, kword):
     f = filename.lower()[:-4]
     def _match(options):
@@ -128,12 +288,12 @@ def main():
     parser.add_argument('--keyword','-k',type=str,help="Add keyword to search among subtitles")
     parser.add_argument('--title','-t',type=str,help="Set the title of the show")
     args = parser.parse_args()
-    lib.setup_logger(lib.LOGGER_LEVEL)
+    setup_logger(LOGGER_LEVEL)
 
     if not args.quiet:
         console = logging.StreamHandler()
-        console.setFormatter(lib.LOGGER_FORMATTER)
-        lib.logger.addHandler(console)
+        console.setFormatter(LOGGER_FORMATTER)
+        logger.addHandler(console)
 
     cursor = FileFinder(args.path, with_extension=_extensions)
 
@@ -158,16 +318,16 @@ def main():
                 title=args.title
             else:
                 title = info["title"]
-            url = lib.get_subtitle_url(
+            url = get_subtitle_url(
                 title, number,
                 metadata,
                 args.choose)
-        except lib.NoResultsError as e:
-            lib.logger.error(str(e))
+        except NoResultsError as e:
+            logger.error(str(e))
             url=''
         if(url !=''):
             with subtitle_renamer(filepath):
-                lib.get_subtitle(url, 'temp__' + filename )
+                 get_subtitle(url, 'temp__' + filename )
 
 
 if __name__ == '__main__':
