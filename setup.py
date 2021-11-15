9bae8614b4bee3537974d4f564d5b01c69f501d4
Spheres_cu <72468068+Spheres-cu@users.noreply.github.com>
2021-11-15 12:37:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open('README.md').read() + '\n' + open('HISTORY.md').read()
 
 setup(
     name='subdivx-downloader',
-    version='0.1',
+    version='0.2',
     description='A command line tool to download the best matching subtitle from subdivx.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,12 +15,12 @@ setup(
     url='https://github.com/Spheres-cu/subdivx-downloader',
     packages=['subdivx',],
     license='GNU GENERAL PUBLIC LICENCE v3.0',
-    install_requires=['beautifulsoup4', 'tvnamer', 'guessit', 'rarfile'],
+    install_requires=['beautifulsoup4', 'tvnamer', 'guessit', 'rarfile', 'colorama'],
     entry_points={
         'console_scripts': ['subdivx=subdivx.cli:main'],
     },
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 2 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License (GPL)',
